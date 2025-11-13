452d2e7f28b2d76d481eae5cecef073ed0d90f82
ernesdumani <ernesdumani@gmail.com>
2025-11-13 19:11:27 +0100

    app building

--- lesson_19/adding_data.py
+++ lesson_19/adding_data.py
@@ -1,9 +1,6 @@
 import streamlit as st
 import pandas as pd
-import
-
-plotly.express
-as px
+import plotly.express as px
 
 # Load the dataset
 books_df = pd.read_csv('bestsellers_with_categories_2022_03_27.csv')
@@ -17,6 +14,73 @@ with st.sidebar.form("book_form"):
     new_name = st.text_input("Book Name")
     new_author = st.text_input("Author")
     new_user_rating = st.slider("User Rating", 0.0, 5.0, 0.0, 0.1)
+    new_reviews = st.number_input("Reviews", min_value=0.0, step=0.5)
+    new_price = st.number_input("Price", min_value=0.0, step=0.5)
+    new_year = st.number_input("Year", min_value=2009, max_value=2025,  step=10)
+    new_genre = st.selectbox("Genre", books_df['Genre'].unique())
+    submit_button = st.form_submit_button(label="Add Book")
+
+
+if submit_button:
+    new_data = {
+        'name': new_name,
+        'author': new_author,
+        'user_rating': new_user_rating,
+        'reviews': new_reviews,
+        'price': new_price,
+        'year': new_year,
+        'genre': new_genre,
+    }
+
+    books_df = pd.concat([pd.DataFrame([new_data]), books_df], ignore_index=True)
+    books_df.to_csv('bestsellers_with_categories_2022_03_27.csv')
+    st.sidebar.success('New books added successfully')
+
+
+
+
+#sidebar filters
+st.sidebar.header("Filter Options")
+selected_author = st.sidebar.selectbox("Select Author", ["All"] + list(books_df['Author'].unique()))
+selected_year = st.sidebar.selectbox("Select Year", ["All"] + list(books_df['Year'].unique()))
+selected_genre = st.sidebar.selectbox("Select Genre", ["All"] + list(books_df['Genre'].unique()))
+min_rating = st.sidebar.slider("Minimum Rating", 0.0, 5.0, 0.0, 0.1)
+max_price = st.sidebar.number_input(
+    "Maximum Price",
+    min_value=float(0),
+    max_value=float(books_df['Price'].max()),
+    value=float(books_df['Price'].max())
+)
+
+
+
+
+
+
+#filter the dataset
+filtered_books_df = books_df.copy()
+if selected_author != "All":
+    filtered_books_df = filtered_books_df[filtered_books_df['Author'] == selected_author]
+if selected_year != "All":
+    filtered_books_df = filtered_books_df[filtered_books_df['Year'] == selected_year]
+if selected_genre != "All":
+    filtered_books_df = filtered_books_df[filtered_books_df['Genre'] == selected_genre]
+
+filtered_books_df = filtered_books_df[
+    (filtered_books_df['User Rating'] >= min_rating) & (filtered_books_df['Price'] <= max_price)
+]
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Summary Statistics
 st.subheader("Summary Statistics")
@@ -60,9 +124,7 @@ st.plotly_chart(fig)
 # Number of Fiction vs Non-Fiction Books Over the Years
 st.subheader("Number of Fiction vs Non-Fiction Books Over the Years")
 size = books_df.groupby(['Year', 'Genre']).size().reset_index(name='Counts')
-fig =
-px.bar
-(size, x='Year', y='Counts', color='Genre', title="Number of Fiction vs Non-Fiction Books from 2009-2022",
+fig = px.bar(size, x='Year', y='Counts', color='Genre', title="Number of Fiction vs Non-Fiction Books from 2009-2022",
 color_discrete_sequence=px.colors.sequential.Plasma, barmode='group')
 st.plotly_chart(fig)
 
@@ -70,9 +132,7 @@ st.plotly_chart(fig)
 st.subheader("Top 15 Authors by Counts of Books Published (2009-2022)")
 top_authors = books_df['Author'].value_counts().head(15).reset_index()
 top_authors.columns = ['Author', 'Count']
-fig =
-px.bar
-(top_authors, x='Count', y='Author', orientation='h',
+fig =px.bar (top_authors, x='Count', y='Author', orientation='h',
 title='Top 15 Authors by Counts of Books Published',
 labels={'Count': 'Counts of Books Published', 'Author': 'Author'},
 color='Count', color_continuous_scale=px.colors.sequential.Plasma)
---
+++ module_20/fileJson.py
@@ -0,0 +1,21 @@
+{
+  "name":"Jane Doe",
+  "age": 25,
+  "address": {
+    "street": "156 Main St",
+    "city": "Anytown",
+    "state": "Texas",
+    "postal_code": "73301"
+  },
+  "contacts" : [
+    {
+      "type" : "email",
+      "value": "jane.doe@gmail.com"
+    },
+    {
+      "type": "phone",
+      "value": "123-32-323"
+
+    }
+  ]
+}
